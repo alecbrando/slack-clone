62fd44d43c10352a9e4351675a93565d90596ae5
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-15 14:45:36 -0700

    Header styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+        <>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+              <AppBody>
+                <Sidebar />
+                <Switch>
+                  <Route path="/" exact></Route>
+                </Switch>
+              </AppBody>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div``;
---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWfvZRsKa-qA149-D8JZjnhSEo4wzcUsU",
+  authDomain: "slack-clone-4b76b.firebaseapp.com",
+  projectId: "slack-clone-4b76b",
+  storageBucket: "slack-clone-4b76b.appspot.com",
+  messagingSenderId: "666643025657",
+  appId: "1:666643025657:web:f6fe392b77fbad58331c28",
+};
+
+export default firebaseConfig;
