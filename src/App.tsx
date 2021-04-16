b2f9e02182e2e69ac048ba47718bd9d202486938
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-16 14:08:04 -0700

    added spinkit for loading

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,23 @@ import Chat from "./components/Chat";
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "./components/Login";
-
+// @ts-ignore
+import Spinner from "react-spinkit";
 function App() {
   const [user, loading] = useAuthState(auth);
-  console.log(user);
+  if (loading) {
+    return (
+      <AppLoadingContainer>
+        <AppLoadingInner>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt="loading"
+          />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingInner>
+      </AppLoadingContainer>
+    );
+  }
   return (
     <div className="App">
       <Router>
@@ -39,6 +52,25 @@ function App() {
 
 export default App;
 
+const AppLoadingContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+const AppLoadingInner = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    height: 100px;
+    padding: 20px;
+    object-fit: contain;
+    margin-bottom: 30px;
+  }
+`;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
